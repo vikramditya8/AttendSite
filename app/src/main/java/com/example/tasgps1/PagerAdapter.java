35f09765f6c3dd6cd05be20925fd6d03c20225b8
vikramditya8 <64085250+vikramditya8@users.noreply.github.com>
2020-08-22 16:36:07 +0530

    Update PagerAdapter.java

--- app/src/main/java/com/example/tasgps1/PagerAdapter.java
+++ app/src/main/java/com/example/tasgps1/PagerAdapter.java
@@ -1,9 +1,4 @@
 package com.example.tasgps1;
-/* Created By Adittya Raj
-  email:raj21adittya@gmail.com
-  This is a personal project(Github Repository)
-  Public Distribution without permission is strictly denied.
- */
 
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
