c8d6238907742820b3b86a177760b3fb0166dacc
vikramditya8 <64085250+vikramditya8@users.noreply.github.com>
2020-08-22 16:35:13 +0530

    Update OriginActivity.java

--- app/src/main/java/com/example/tasgps1/OriginActivity.java
+++ app/src/main/java/com/example/tasgps1/OriginActivity.java
@@ -1,10 +1,4 @@
 package com.example.tasgps1;
-/* Created By Adittya Raj
-  email:raj21adittya@gmail.com
-  This is a personal project(Github Repository)
-  Public Distribution without permission is strictly denied.
- */
-
 import android.net.Uri;
 
 import com.google.android.material.tabs.TabLayout;
